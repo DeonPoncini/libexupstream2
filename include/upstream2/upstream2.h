26b0a0a33e8734fd9a054f65b69b044eb8b6c096
Deon Poncini <dex1337@gmail.com>
2014-11-29 18:28:33 -0800

    Simple upstream library
    Built with the new build system

---
+++ include/upstream2/upstream2.h
@@ -0,0 +1,11 @@
+#ifndef UPSTREAM2_H
+#define UPSTREAM2_H
+
+namespace upstream2
+{
+
+int upstream_version();
+
+}
+
+#endif
---
+++ src/upstream2.cpp
@@ -0,0 +1,11 @@
+#include "upstream2/upstream2.h"
+
+namespace upstream2
+{
+
+int upstream_version()
+{
+    return 2;
+}
+
+}
